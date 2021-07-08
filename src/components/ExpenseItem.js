39f46971972f5a014ee3a6d291b4f6401d0860c0
Carl Wilke <carlwilke@outlook.com>
2021-07-08 18:18:49 +0200

    Added props to Expense Items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return  (
-        <div className="expense-item">
-            <div> July 8th, 2021</div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$295.15</div>
-            </div>
-        </div>
-    );
-    
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

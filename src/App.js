0df4c51b6c0c2489dae6c1dd68a188e57bb6ef07
Carl Wilke <carlwilke@outlook.com>
2021-07-19 16:28:37 +0200

    Data update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Expenses from "./components/Expenses";
 const expenses = [
   {
     id: "e1",
-    title: "Bog Rolls",
-    amount: 94.12,
+    title: "Curry of Doom",
+    amount: 44.12,
     date: new Date(2020, 7, 14),
   },
   {
     id: "e2",
-    title: "Curry Inferno",
+    title: "Bog Rolls",
     amount: 99.49,
     date: new Date(2021, 2, 12),
   },

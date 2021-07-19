a01c3eae9fb9e8c97d9adc41522e1d533c13270a
Carl Wilke <carlwilke@outlook.com>
2021-07-19 10:52:22 +0200

    Added ExpenseDate

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 const expenses = [
   {
     id: "e1",
-    title: "Toilet Paper",
+    title: "Bog Rolls",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
@@ -35,17 +35,17 @@ function App() {
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
